964f3e9bbe66a3673f987ff18f4f8ef79c7e8dd6
Ryan Borel <rjborel@uno.edu>
2025-01-31 12:48:47 -0600

    navBar complete and other pages implemented

---
+++ navbar.js
@@ -0,0 +1,22 @@
+//automatically highlight the link of the current page
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-links a"); // Select all navigation links
+    let currentPath = window.location.pathname;
+
+    console.log("current path: " + currentPath);
+    // If the current path "/", treat it as a "index.html"
+    if (currentPath === "/index.html") {
+        currentPath = currentPath.split("/").pop();
+    } 
+    
+
+    console.log("current path: " + currentPath);
+    console.log(window.location.href);
+
+    navLinks.forEach(link => {
+       if(link.getAttribute("href") === currentPath){
+        console.log("working");
+        link.classList.add("active");
+       }
+    });
+});
