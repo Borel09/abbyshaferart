73c394e000c7a0a5d92136ae8e451c131329ead1
Ryan Borel <rjborel@uno.edu>
2025-02-01 01:47:11 -0600

    homepage & navbar update

--- home.js
+++ home.js
@@ -9,3 +9,17 @@ window.addEventListener("scroll", function(){
         sigBox.classList.remove("visible");
     }
 });
+
+function openImage(src){
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    lightboxImg.src = src;
+    lightbox.style.display = "flex";
+   
+
+}
+function closeImage(){
+    document.getElementById("lightbox").style.display = "none";
+    
+}
