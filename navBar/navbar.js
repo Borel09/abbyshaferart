0155a179a3f112ef71c904ab98c130789fd5c038
Ryan Borel <rjborel@uno.edu>
2025-02-05 18:04:56 -0600

    fix navbar gallery dropdown

--- navBar/navbar.js
+++ navBar/navbar.js
@@ -3,33 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll(".nav-links a"); // Select all navigation links
     let currentPath = window.location.pathname;
 
-    
-    console.log(currentPath)
-
-    if (currentPath === "/index.html") {
-        currentPath = currentPath.split("/").pop();
-    } 
+    let galleryParent = document.querySelector('.nav-links .dropdown > p');
+    let isGalleryPage = false;
 
     navLinks.forEach(link => {
-       if(link.getAttribute("href") === currentPath){
-        link.classList.add("active");
-       }
-       
-       if(link.getAttribute("href") === "/architecture/architectureSeries.html" || 
-            link.getAttribute("href") === "/otherWorks/otherWorks.html"){
-        link.classList.remove("active");
-       }
+        let linkPath = new URL(link.href).pathname;
+        console.log(linkPath)
+
+        if(linkPath === currentPath){
+            link.classList.add("active");
+        }
+
+        if(linkPath.startsWith("/architecture/") && linkPath === currentPath){
+            link.classList.add("active-dropdown");
+        } else if(linkPath.startsWith("/otherWorks/") && linkPath === currentPath){
+            link.classList.add("active-dropdown");
+        }
 
     });
+    // if current page belongs to the gallery dropdown, highlight the main "gallery" link
+    if(isGalleryPage && galleryParent){
+        galleryParent.classList.add("active");
+    }
 });
 
 
 
 var navbar = document.getElementById("navBar");
 window.addEventListener("scroll", function(){
-    if(window.scrollY > 0) {
-        navbar.classList.add("fixed", "shrink");
-    } else {
-        navbar.classList.remove("fixed", "shrink");
-    }
+    navbar.classList.toggle("fixed", window.scrollY > 0);
+    navbar.classList.toggle("shrink", window.scrollY > 0);
 });
